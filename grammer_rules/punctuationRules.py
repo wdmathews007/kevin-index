dc49eea031d5e34ff23faf8172f3f3077dcd12c8
myron <mquijano.edu@gmail.com>
2026-03-20 12:50:44 -0400

    Filled out the punctuation frequencies

--- grammer_rules/punctuationRules.py
+++ grammer_rules/punctuationRules.py
@@ -1,4 +1,4 @@
-from collections import Counter 
+#from collections import Counter 
 
 def splitter(n, s):
     pieces = s.split()
@@ -6,17 +6,119 @@ def splitter(n, s):
 
 text = "12; 3456 124 345 23 45;"
 
-def instance_of_semicolon(text):
+def semicolonFreq(text):
     num = 0
-    freq = Counter(text)
+    numList = []
+    avg = 0
 
     for i in splitter(100, text):
-        num += i.count(';')
+        numList.append(i.count(";"))
+        num += 1
+    
+    for i in numList:
+        avg += i
 
-    return num 
-        
-        
+    avg = avg/num
 
+    return avg
+
+def commaFreq(text):
+        num = 0
+        numList = []
+        avg = 0
+
+        for i in splitter(100, text):
+            numList.append(i.count(","))
+            num += 1
+    
+        for i in numList:
+            avg += i
+
+        avg = avg/num
+
+        return avg
+
+def emmdashFreq(text):
+    num = 0
+    numList = []
+    avg = 0
+
+    for i in splitter(100, text):
+        numList.append(i.count("—"))
+        num += 1
+    
+    for i in numList:
+        avg += i
+
+    avg = avg/num
+
+    return avg    
+
+def ellipsisFreq(text):
+    num = 0
+    numList = []
+    avg = 0
+
+    for i in splitter(100, text):
+        numList.append(i.count("..."))
+        num += 1
+    
+    for i in numList:
+        avg += i
+
+    avg = avg/num
+
+    return avg  
+
+def exclamationFreq(text):
+    num = 0
+    numList = []
+    avg = 0
+
+    for i in splitter(100, text):
+        numList.append(i.count("!"))
+        num += 1
+    
+    for i in numList:
+        avg += i
+
+    avg = avg/num
+
+    return avg  
+
+def colonFreq(text):
+    num = 0
+    numList = []
+    avg = 0
+
+    for i in splitter(100, text):
+        numList.append(i.count(":"))
+        num += 1
+    
+    for i in numList:
+        avg += i
+
+    avg = avg/num
+
+    return avg  
+
+def parenthesesFreq(text):
+    num = 0
+    numList = []
+    avg = 0
+
+    for i in splitter(100, text):
+        numList.append(i.count("("))
+        num += 1
+    
+    for i in numList:
+        avg += i
+
+    avg = avg/num
+
+    return avg  
+
+    
 if __name__ == "__main__":
-    number = instance_of_semicolon(text)
+    number = semicolonFreq(text)
     print(number)
